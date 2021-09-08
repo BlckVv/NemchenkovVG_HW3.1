9ff884d3f925d360384acb673182dbdb6c00bed7
NemchenkovVG <blckvv@yandex.ru>
2021-09-09 00:36:52 +0400

    Initial Commit

---
+++ NemchenkovVG_HW3.1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NemchenkovVG_HW3.1
+//
+//  Created by Владимир Немченков on 09.09.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NemchenkovVG_HW3.1/NemchenkovVG_HW3_1App.swift
@@ -0,0 +1,17 @@
+//
+//  NemchenkovVG_HW3_1App.swift
+//  NemchenkovVG_HW3.1
+//
+//  Created by Владимир Немченков on 09.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct NemchenkovVG_HW3_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
